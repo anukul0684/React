2f12aa3f8b31e9beff72d949726ffbf3604b0d41
anukul0684 <68563638+anukul0684@users.noreply.github.com>
2021-07-01 16:37:57 -0500

    inline style in jsx code

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,30 @@ import ReactDOM from "react-dom";
 
 //Create a React Component
 const App = () => {
-  return <div> Hi there!</div>;
+  // arrow function or traditional like const App = function() { };
+  return (
+    /* return returns jsx code */
+    <div>
+      {/* This is jsx comment */}
+      <label for="name" class="label">
+        Enter Name
+      </label>
+      <input id="name" type="text" />
+      <button
+        style={{
+          backgroundColor: "#55f",
+          color: "#fff",
+          border: "1px solid #000",
+        }}
+      >
+        Submit
+      </button>
+      {/* first {} states that we want to reference a js variable inside our jsx */}
+      {/* second {} states that we want to add a js object having keys and values, 
+                and just like js we have css properties written same as in js code*/}
+    </div>
+  );
+  //   <div> Hi there!</div>;
 };
 
 //Take the React Component and show it on the screen
