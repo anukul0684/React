88e282304327bd2656c48c91de2c69c7c0c28af9
anukul0684 <68563638+anukul0684@users.noreply.github.com>
2021-07-01 17:07:05 -0500

    jsx vs html

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,14 +2,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText() {
+  return "Click on Me!";
+}
+
 //Create a React Component
 const App = () => {
   // arrow function or traditional like const App = function() { };
+
+  /* Limitations in referencing js variables:
+       We are not allowed to use a js object and reference it in JSX as js objects are not valid as a React child 
+       React child means showing js object inside a React component and React does not know how to handle it*/
+  //object
+  const buttonText = { text: "Click Me" };
+
+  //text
+  //const buttonText = [10, 20];
+  //const buttonText = ["Hi", "There"];
+  //const buttonText = "Click Me";
+  //const buttonText = 123456;
+
   return (
     /* return returns jsx code */
     <div>
       {/* This is jsx comment */}
-      <label for="name" class="label">
+      {/* Diff between HTML and JSX 1: Instead of class in JSX we write className as class is a js keyword. 
+          To avoid confusion, we use className since old times */}
+      <label for="name" className="label">
         Enter Name
       </label>
       <input id="name" type="text" />
@@ -20,11 +39,21 @@ const App = () => {
           border: "1px solid #000",
         }}
       >
-        Submit
+        {/* {getButtonText()} */}
+        {/* {} not indicating js object, instead it means we want to reference a js variable here.  */}
+        {buttonText.text}{" "}
+        {/** correct way to get js object by calling its property */}
+        {/* Submit */}
       </button>
-      {/* first {} states that we want to reference a js variable inside our jsx */}
+
+      {/* Diff between HTML and JSX 2:Converting inline style to JSX format
+          first {} states that we want to reference a js variable inside our jsx */}
       {/* second {} states that we want to add a js object having keys and values, 
                 and just like js we have css properties written same as in js code*/}
+
+      {/* half community convention and half jsx convention. put non-jsx in '' and jsx in "" */}
+
+      {/* Diff between HTML and JSX 3: JSX referencing js variables */}
     </div>
   );
   //   <div> Hi there!</div>;
