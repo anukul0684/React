eb07e6944e29833b0aa6d5372ecfb47b1b3d91b7
anukul0684 <68563638+anukul0684@users.noreply.github.com>
2021-07-01 20:06:16 -0500

    naive approach for duplicated data

--- react_components/src/index.js
+++ react_components/src/index.js
@@ -1,10 +1,57 @@
 //Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
+//import faker
+import faker from "faker";
 
 //Create a React Component
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {faker.name.firstName()}
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 PM</span>
+          </div>
+          <div className="text">Nice Blog Post!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {faker.name.firstName()}
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 PM</span>
+          </div>
+          <div className="text">Nice Blog Post!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {faker.name.firstName()}
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 PM</span>
+          </div>
+          <div className="text">Nice Blog Post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 //Take the React Component and show it on the screen
