993561972c182eead41240a1c810a8a3841445f5
anukul0684 <68563638+anukul0684@users.noreply.github.com>
2021-07-10 19:13:00 -0500

    use of props to configure child element

--- react_components/src/CommentDetail.js
+++ react_components/src/CommentDetail.js
@@ -1,21 +1,22 @@
 import React from "react";
 //import faker
-import faker from "faker";
+//import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+  console.log(props);
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.authorImage} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00 PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice Blog Post!</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
--- react_components/src/index.js
+++ react_components/src/index.js
@@ -15,12 +15,42 @@ const App = () => {
     <div className="ui container comments">
       {/* if using component inside a component, it has to be called as JSX tag like HTML element tag. */}
       {/* Child/Nested components: */}
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        authorImage={faker.image.avatar()}
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        content="Nice Blog Post"
+      />
+      <CommentDetail
+        authorImage={faker.image.avatar()}
+        author="Alex"
+        timeAgo="Yesterday at 2:00PM"
+        content="Well said!"
+      />
+      <CommentDetail
+        authorImage={faker.image.avatar()}
+        author="Jane"
+        timeAgo="On Dec 12, 2020 at 1:00PM"
+        content="Totally agree..."
+      />
+      <CommentDetail
+        authorImage={faker.image.avatar()}
+        author="Simon"
+        timeAgo="On Monday Jul 05, 2021 at 2:00PM"
+        content="Keep sharing"
+      />
+      <CommentDetail
+        authorImage={faker.image.avatar()}
+        author="Rose"
+        timeAgo="On Tuesday Jul 06, 2021 at 2:00PM"
+        content="Keep it up!"
+      />
+      <CommentDetail
+        authorImage={faker.image.avatar()}
+        author="Jasmine"
+        timeAgo="On Thursday Jul 08, 2021 at 2:00PM"
+        content="I love your ideas"
+      />
     </div>
   );
 };
