3ee7bfeda0ac081784a804b7741425edceb1e540
anukul0684 <68563638+anukul0684@users.noreply.github.com>
2021-07-10 22:41:21 -0500

    finalized design and nesting components. Created new project

--- react_components/src/index.js
+++ react_components/src/index.js
@@ -13,46 +13,76 @@ const App = () => {
   //Props/properties - System for passing data from a PARENT Component to a CHILD Component to customize/configure a CHILD Component
   // customize how the component look or how the user interacts with them
   return (
-    <div className="ui container comments">
+    <div className="ui stackable grid container comments">
       {/* if using component inside a component, it has to be called as JSX tag like HTML element tag. */}
       {/* Child/Nested components: */}
-      <ApprovalCard />
-      <CommentDetail
-        authorImage={faker.image.avatar()}
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Nice Blog Post"
-      />
-      <CommentDetail
-        authorImage={faker.image.avatar()}
-        author="Alex"
-        timeAgo="Yesterday at 2:00PM"
-        content="Well said!"
-      />
-      <CommentDetail
-        authorImage={faker.image.avatar()}
-        author="Jane"
-        timeAgo="On Dec 12, 2020 at 1:00PM"
-        content="Totally agree..."
-      />
-      <CommentDetail
-        authorImage={faker.image.avatar()}
-        author="Simon"
-        timeAgo="On Monday Jul 05, 2021 at 2:00PM"
-        content="Keep sharing"
-      />
-      <CommentDetail
-        authorImage={faker.image.avatar()}
-        author="Rose"
-        timeAgo="On Tuesday Jul 06, 2021 at 2:00PM"
-        content="Keep it up!"
-      />
-      <CommentDetail
-        authorImage={faker.image.avatar()}
-        author="Jasmine"
-        timeAgo="On Thursday Jul 08, 2021 at 2:00PM"
-        content="I love your ideas"
-      />
+      <div className="centered row">
+        <ApprovalCard>
+          <div>
+            <h4>Warning!</h4>Are you Sure you want to do this?
+          </div>
+        </ApprovalCard>
+      </div>
+      <div className="four wide column">
+        <ApprovalCard>
+          <CommentDetail
+            authorImage={faker.image.avatar()}
+            author="Sam"
+            timeAgo="Today at 4:45PM"
+            content="Nice Blog Post"
+          />
+        </ApprovalCard>
+      </div>
+      <div className="four wide column">
+        <ApprovalCard>
+          <CommentDetail
+            authorImage={faker.image.avatar()}
+            author="Alex"
+            timeAgo="Yesterday at 2:00PM"
+            content="Well said!"
+          />
+        </ApprovalCard>
+      </div>
+      <div className="four wide column">
+        <ApprovalCard>
+          <CommentDetail
+            authorImage={faker.image.avatar()}
+            author="Jane"
+            timeAgo="On Dec 12, 2020 at 1:00PM"
+            content="Totally agree..."
+          />
+        </ApprovalCard>
+      </div>
+      <div className="four wide column">
+        <ApprovalCard>
+          <CommentDetail
+            authorImage={faker.image.avatar()}
+            author="Simon"
+            timeAgo="On Monday Jul 05, 2021 at 2:00PM"
+            content="Keep sharing"
+          />
+        </ApprovalCard>
+      </div>
+      <div className="four wide column">
+        <ApprovalCard>
+          <CommentDetail
+            authorImage={faker.image.avatar()}
+            author="Rose"
+            timeAgo="On Tuesday Jul 06, 2021 at 2:00PM"
+            content="Keep it up!"
+          />
+        </ApprovalCard>
+      </div>
+      <div className="four wide column">
+        <ApprovalCard>
+          <CommentDetail
+            authorImage={faker.image.avatar()}
+            author="Jasmine"
+            timeAgo="On Thursday Jul 08, 2021 at 2:00PM"
+            content="I love your ideas"
+          />
+        </ApprovalCard>
+      </div>
     </div>
   );
 };
