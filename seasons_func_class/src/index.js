208f545915716147bd7d296f3c246897e1a50071
anukul0684 <68563638+anukul0684@users.noreply.github.com>
2021-07-11 21:21:48 -0500

    creating a class component

--- seasons_func_class/src/index.js
+++ seasons_func_class/src/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position), //gets the latitude and longitude
-    (err) => console.log(err) //gets the error message when unable to find geo location
-  );
-  return <div>Hi there!</div>;
-};
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position), //gets the latitude and longitude
+//     (err) => console.log(err) //gets the error message when unable to find geo location
+//   );
+//   return <div>Latitude: </div>;
+// };
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position), //gets the latitude and longitude
+      (err) => console.log(err) //gets the error message when unable to find geo location
+    );
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
