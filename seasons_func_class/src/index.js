0a9c1efca9c70d43696fb469edbf59ca7ba45bf0
anukul0684 <68563638+anukul0684@users.noreply.github.com>
2021-08-04 22:23:48 -0500

    Seasons page as per month and latitude and Page loading completed

--- seasons_func_class/src/index.js
+++ seasons_func_class/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 // const App = () => {
 //   window.navigator.geolocation.getCurrentPosition(
@@ -17,50 +19,67 @@ class App extends React.Component {
   //so whenever we will create an instance of this App component, this function will be called first automatically.
   //A magic function that is automatically and instantly called before anything else when ever the instance of this class is created.
   //constructor() is automatically called with props object. The same object that we saw in our functional component.
-  constructor(props) {
-    //we can use this function to initialize 'state'. There are another ways as well to initialize 'state'
-    //before we do anything inside this function, we have to do one ceremonial step. As it is required because we have to do this.
-    //we have to call a function called super() with props as its argument.
 
-    super(props);
+  //constructor(props) { //**********************We don't need the constructor anymore********************
 
-    //this is the only time we do direct assignment to this.state. After this use setState() to do the change, update or manipulate our this.state object's property.
-    this.state = {
-      latitude: null, //default it to a sensible value which is here is a number
-      longitude: null,
-      errorMessage: "",
-    };
+  //we can use this function to initialize 'state'. There are another ways as well to initialize 'state'
+  //before we do anything inside this function, we have to do one ceremonial step. As it is required because we have to do this.
+  //we have to call a function called super() with props as its argument.
+  //super(props); //********************We don't need the constructor anymore********************
 
-    // this.state ={} is our state object that is going to eventually contain some different pieces of data and some different property that are very imp and relevant to our component that we are putting together
+  //this is the only time we do direct assignment to this.state. After this use setState() to do the change, update or manipulate our this.state object's property.
+  // this.state = { //*************************We don't need the constructor anymore********************
+  //   latitude: null, //default it to a sensible value which is here is a number
+  //   longitude: null,
+  //   errorMessage: "",
+  // };
 
+  // this.state = {} is our state object that is going to eventually contain some different pieces of data and some different property that are very imp and relevant to our component that we are putting together
+  // Note - componentDidMount() - putting below code in there instead of here.
+  // Note - constructor(props) putting this function here instead of render() to avoid multiple times calling as render is called many times.
+  // window.navigator.geolocation.getCurrentPosition(
+  //   (position) => {
+  //     //console.log(position)
+  //     //we call setState
+  //     this.setState({ latitude: position.coords.latitude });
+  //     this.setState({ longitude: position.coords.longitude });
+  //     //never ever do this below thing
+  //     //this.state.latitude = position.coords.latitude;
+  //   }, //gets the latitude and longitude
+  //   (err) => {
+  //     console.log(err); //gets the error message when unable to find geo location
+  //     this.setState({
+  //       errorMessage:
+  //         err.message +
+  //         " Some Error Occurred. We are sorry. We are unable to provide Latitude and Longitude this moment.",
+  //     });
+  //   }
+  // );
+  //}
+
+  // ************* We don't need the constructor anymore. Below is the condensed code equivalent to constructor function and initializing state  ********************
+  state = { latitude: null, longitude: null, errorMessage: "" };
+
+  //componentDidMount() is called one time immediately after the render() method is called when the component instance is created
+  componentDidMount() {
+    console.log("My component was rendered to the screen");
     // putting this function here instead of render() to avoid multiple times calling as render is called many times.
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //console.log(position)
-        //we call setState
-        this.setState({ latitude: position.coords.latitude });
-        this.setState({ longitude: position.coords.longitude });
-
-        //never ever do this below thing
-        //this.state.latitude = position.coords.latitude;
-      }, //gets the latitude and longitude
-      (err) => {
-        console.log(err); //gets the error message when unable to find geo location
+      (position) =>
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }), //gets the latitude and longitude
+      (err) =>
         this.setState({
           errorMessage:
             err.message +
             " Some Error Occurred. We are sorry. We are unable to provide Latitude and Longitude this moment.",
-        });
-      }
+        }) //gets the error message or we say handling errors gracefully
     );
   }
 
-  //componentDidMount() is called one time immediately after the render() method is called when the component instance is created
-  // componentDidMount() {
-  //   console.log("My component was rendered to the screen");
-  // }
-
-  //componentDidUpdate() is called everytime after the re-render occurs. So technically render() method sits before this method.
+  //componentDidUpdate() is called everytime after the re-render occurs. So technically render() method is called before this method.
   // componentDidUpdate() {
   //   console.log("My component got updated - it re-rendered");
   // }
@@ -90,17 +109,22 @@ class App extends React.Component {
       (this.state.latitude || this.state.longitude)
     ) {
       return (
-        <div>
-          <h1>Latitude: {this.state.latitude}</h1>
-          <h1>Longitude: {this.state.longitude}</h1>
-        </div>
+        <SeasonDisplay
+          latitude={this.state.latitude}
+          longitude={this.state.longitude}
+        />
+        // <div>
+        //   <h1>Latitude: {this.state.latitude}</h1>
+        //   <h1>Longitude: {this.state.longitude}</h1>
+        // </div>
       );
     }
 
     return (
-      <div>
-        <h1>Page is Loading. Please wait...</h1>
-      </div>
+      <Spinner />
+      // <div>
+      //   <h1>Page is Loading. Please wait...</h1>
+      // </div>
     );
   }
 }
